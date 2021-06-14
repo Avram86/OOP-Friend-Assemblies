d3fb9532e5e7ca00bed10c4a5969e45e1e7b594a
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-06-14 19:22:06 +0300

    Add project files.

---
+++ OOP-Friend-Assemblies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Friend_Assemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
